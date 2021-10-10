966c6748c745abf4866cd94d38e2d7d840935b0f
Murat Altınpınar <murtekbey@gmail.com>
2021-10-11 02:40:21 +0300

    readme added

--- src/main/java/pribas/task/api/MomentsController.java
+++ src/main/java/pribas/task/api/MomentsController.java
@@ -2,23 +2,31 @@ package pribas.task.api;
 
 import java.util.Date;
 import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import pribas.task.business.abstracts.MomentService;
 import pribas.task.core.utilities.results.DataResult;
+import pribas.task.core.utilities.results.ErrorDataResult;
 import pribas.task.core.utilities.results.Result;
 import pribas.task.entities.Moment;
 
@@ -82,7 +90,14 @@ public class MomentsController {
 		return this.momentService.findByMomentDateBetween(from, to);
 	}
 	
-	
-	
-	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	public ErrorDataResult<Object> handleValidationException(MethodArgumentNotValidException exceptions) {
+		Map<String, String> validationErrors = new HashMap<String, String>();
+		for(FieldError fieldError: exceptions.getBindingResult().getFieldErrors()) {
+			validationErrors.put(fieldError.getField(), fieldError.getDefaultMessage());
+		}
+		ErrorDataResult<Object> errors = new ErrorDataResult<Object>(validationErrors, "Validation errors");
+		return errors;
+	}
 }
--- src/main/java/pribas/task/api/TimelinesController.java
+++ src/main/java/pribas/task/api/TimelinesController.java
@@ -1,21 +1,29 @@
 package pribas.task.api;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import pribas.task.business.abstracts.TimelineService;
 import pribas.task.core.utilities.results.DataResult;
+import pribas.task.core.utilities.results.ErrorDataResult;
 import pribas.task.core.utilities.results.Result;
 import pribas.task.entities.Timeline;
 
@@ -69,5 +77,16 @@ public class TimelinesController {
 	@GetMapping("/getByTagsIn")
 	public DataResult<List<Timeline>> getByTagsIn(@Valid @RequestParam List<String> tags) {
 		return this.timelineService.getByTagsIn(tags);
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	public ErrorDataResult<Object> handleValidationException(MethodArgumentNotValidException exceptions) {
+		Map<String, String> validationErrors = new HashMap<String, String>();
+		for(FieldError fieldError: exceptions.getBindingResult().getFieldErrors()) {
+			validationErrors.put(fieldError.getField(), fieldError.getDefaultMessage());
+		}
+		ErrorDataResult<Object> errors = new ErrorDataResult<Object>(validationErrors, "Validation errors");
+		return errors;
 	}
 }
